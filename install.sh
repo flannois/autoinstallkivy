a8b55e47a99833431db57e0fe571a9a7a62dedea
kaox <kaox@debian-pc.home>
2020-06-06 20:19:21 +0200

    install auto kivy dependencies

---
+++ install.sh
@@ -0,0 +1,29 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y python3-pip \
+git \
+zlib1g-dev \
+openjdk-11-jdk \
+pkg-config \
+autoconf \
+automake \
+build-essential \
+autoconf \
+libtool \
+lld \
+libffi-dev \
+python3-dev \
+libssl-dev \
+cython3 \
+cython \
+llvm \
+zip \
+zipalign \
+apksigner
+sudo pip3 install --upgrade pip setuptools virtualenv
+sudo pip3 install kivy \
+kivy_examples \
+ffpyplayer \
+Cython \
+buildozer \
+jnius
